422f048365d187489e64357fdf758a667304f31c
bjeld <martinbjeld@gmail.com>
2023-01-15 16:17:32 +0100

    Initial Commit

---
+++ SumAnimation/SumAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SumAnimationApp.swift
+//  SumAnimation
+//
+//  Created by Martin Bjeld on 15/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct SumAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
